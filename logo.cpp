14a3230b2ba13152a6e5080863b1937cb5e53842
frumplecoin <70377706+frumplecoin@users.noreply.github.com>
2020-09-21 11:06:16 +0100

    Update.

--- logo.cpp
+++ logo.cpp
@@ -4,15 +4,11 @@ namespace input {
 
     //%
     void onLogoPressed(Action body) {
-        // Forces the PIN to switch to makey-makey style detection.
-        uBit.io.face.isTouched();
         registerWithDal(uBit.io.face.id, MICROBIT_BUTTON_EVT_CLICK, body);
     }
 
     //%
     void onLogoReleased(Action body) {
-        // Forces the PIN to switch to makey-makey style detection.
-        uBit.io.face.isTouched();
         registerWithDal(uBit.io.face.id, MICROBIT_BUTTON_EVT_UP, body);
     }
 
