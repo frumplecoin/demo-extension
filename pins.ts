77efca5bbccce63dcedca08e48ff82a7496b9f33
frumplecoin <70377706+frumplecoin@users.noreply.github.com>
2020-09-23 15:45:20 +0100

    Update.

--- pins.ts
+++ pins.ts
@@ -5,7 +5,7 @@ namespace pins {
     * P0, P1, P2 use resistive touch by default.
     * The logo uses capacitative touch by default.
     * @param name target to change the touch mode for, eg: TouchTarget.P0
-    * @param mode the touch mode to use, eg: TouchMode.Capacitative
+    * @param mode the touch mode to use, eg: TouchTargetMode.Capacitative
     */
     //% help=pins/touch-set-mode weight=60
     //% blockId=device_touch_set_type block="set %name to touch mode %mode"
